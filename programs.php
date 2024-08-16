eb2037f6b39ddf6d8e7ee829e845aca4e553cbcf
LyschevIvan <ivan030400@gmail.com>
2024-08-16 14:35:35 +0300

    Поменял документ

--- programs.php
+++ programs.php
@@ -4,7 +4,7 @@
 <main>
   <h2>Образовательные программы</h2>
   <p>Здесь вы можете ознакомиться с нашими образовательными программами.</p>
-  <iframe src="static/ПО-ПРОГРАММА_КОСМЕТИК.pdf" width="100%" height="600px"></iframe>
+  <iframe src="static/c_doc03806220240815113505.pdf" width="100%" height="600px"></iframe>
 </main>
 
 <?php include('footer.php'); ?>
