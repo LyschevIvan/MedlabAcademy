18f0093d1ba23612316e6d7acf340f09b9982166
LyschevIvan <ivan030400@gmail.com>
2024-08-27 17:09:41 +0300

    Добавление https

---
+++ php/programs.php
@@ -0,0 +1,10 @@
+<!-- programs.php -->
+<?php include('header.php'); ?>
+
+<main>
+  <h2>Образовательные программы</h2>
+  <p>Здесь вы можете ознакомиться с нашими образовательными программами.</p>
+  <iframe src="static/c_doc03806220240815113505.pdf" width="100%" height="600px"></iframe>
+</main>
+
+<?php include('footer.php'); ?>
